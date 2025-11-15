f05afe6a1fd03be4bccf834da41c0e5ca9c9aa1c
Harsh Singh <harsh945455@gmail.com>
2025-11-15 10:16:08 +0530

    Update apipaths.js

--- Frontend/src/utilis/apipaths.js
+++ Frontend/src/utilis/apipaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://miniprjct-healthcare-2.onrender.com/";
 export const API_PATHS = {
     AUTH: {
        REGISTER: "/api/auth/register",
